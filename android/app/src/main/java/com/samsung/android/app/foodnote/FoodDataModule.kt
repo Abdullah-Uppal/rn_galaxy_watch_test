9f1e5a28260d938d225a23ff056b8b9b91387279
Abdullah Uppal <muhammadabdullahupal@gmail.com>
2024-01-26 23:57:11 +0500

    import error fix

--- android/app/src/main/java/com/samsung/android/app/foodnote/FoodDataModule.kt
+++ android/app/src/main/java/com/samsung/android/app/foodnote/FoodDataModule.kt
@@ -9,6 +9,7 @@ import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
+import com.facebook.react.bridge.WritableMap
 import com.facebook.react.modules.core.DeviceEventManagerModule
 import com.samsung.android.app.foodnote.data.DailyIntakeCalories
 import com.samsung.android.sdk.healthdata.HealthConnectionErrorResult
